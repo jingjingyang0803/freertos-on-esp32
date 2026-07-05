0dce6c84161af1e4c069245bcd3a862709a411ed
Jingjing Yang <jingjing.yang@tuni.fi>
2026-07-06 00:18:59 +0300

    Accumulate encoder state transitions into a single rotation event

--- ESP32_ControlPanel/encoder_task.cpp
+++ ESP32_ControlPanel/encoder_task.cpp
@@ -6,6 +6,7 @@
 static QueueHandle_t encoder_queue;
 
 static volatile uint8_t last_state = 0;
+static volatile int8_t encoder_accum = 0;
 
 static void IRAM_ATTR encoder_isr() {
   uint8_t a = digitalRead(ENCODER_PIN_A);
@@ -23,6 +24,11 @@ static void IRAM_ATTR encoder_isr() {
     00         | 01            | CW
     01         | 11            | CW
     11         | 10            | CW
+    10         | 00            | CW
+    00         | 10            | CCW
+    10         | 11            | CCW
+    11         | 01            | CCW
+    01         | 00            | CCW
   */
 
   if ((last_state == 0b00 && current_state == 0b01) ||
@@ -42,8 +48,23 @@ static void IRAM_ATTR encoder_isr() {
   last_state = current_state;
 
   if (valid_event) {
-    BaseType_t higher_priority_task_woken = pdFALSE;
+    if (event.type == INPUT_EVENT_ENCODER_CW) {
+      encoder_accum++;
+    } else {
+      encoder_accum--;
+    }
 
+    if (encoder_accum >= 4) {
+      event.type = INPUT_EVENT_ENCODER_CW;
+      encoder_accum = 0;
+    } else if (encoder_accum <= -4) {
+      event.type = INPUT_EVENT_ENCODER_CCW;
+      encoder_accum = 0;
+    } else {
+      return;
+    }
+
+    BaseType_t higher_priority_task_woken = pdFALSE;
     xQueueSendFromISR(encoder_queue, &event, &higher_priority_task_woken);
 
     if (higher_priority_task_woken == pdTRUE) {
@@ -52,7 +73,7 @@ static void IRAM_ATTR encoder_isr() {
   }
 }
 
-void encoder_init() {
+void encoder_task_start() {
   pinMode(ENCODER_PIN_A, INPUT_PULLUP);
   pinMode(ENCODER_PIN_B, INPUT_PULLUP);
 
@@ -68,5 +89,3 @@ void encoder_init() {
 }
 
 QueueHandle_t encoder_get_queue() { return encoder_queue; }
-
-void encoder_task_start() { encoder_init(); }
--- ESP32_ControlPanel/encoder_task.h
+++ ESP32_ControlPanel/encoder_task.h
@@ -2,15 +2,6 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
 
-typedef enum {
-  ENCODER_EVENT_CW,
-  ENCODER_EVENT_CCW,
-} encoder_event_type_t;
-
-typedef struct {
-  encoder_event_type_t type;
-} encoder_event_t;
-
 QueueHandle_t encoder_get_queue();
 
 void encoder_task_start();
